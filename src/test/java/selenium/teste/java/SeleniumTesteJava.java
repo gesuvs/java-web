0e6d581ef4c4ecac8bbf2acfc36b50662148d695
GBernardo10 <g_bernardo10@hotmail.com>
2019-06-18 14:52:03 -0300

    script de teste acesso login, tarefa concluida realizar insercao da senha atraves do teclado

--- src/test/java/selenium/teste/java/SeleniumTesteJava.java
+++ src/test/java/selenium/teste/java/SeleniumTesteJava.java
@@ -35,57 +35,57 @@ public class SeleniumTesteJava {
         /* configurando o webdriver do Internet Explorer */
         // na linha abaixo, é informado o driver do IE e o caminho que ele se encontra
         System.setProperty("webdriver.ie.driver",
-                "C:\\Users\\aluno\\Downloads\\IEDriverServer.exe");
+                "C:\\Users\\gbjesus1\\Documents\\Selenium\\WebDriver\\IEDriverServer_Win32_3.14.0\\IEDriverServer.exe");
 //        System.setProperty("webdriver.chrome.driver", "C:\\Users\\aluno\\Downloads\\chromedriver_1.exe");
         // Instanciando um objeto chamado driver do tipo WebDriver do Internet Explorer
         WebDriver driver = new InternetExplorerDriver();
 //        WebDriver driver = new ChromeDriver();
         //Informando a URL do site que sera usado para teste.
-        String baseUrl = "https://govconta.caixa.gov.br/sigov/index.do";
+        String baseUrl = "https://10.116.83.101:8010/sigov/index.do";
         //Executando o selenium
         driver.get(baseUrl);
 
         JavascriptExecutor executor = (JavascriptExecutor) driver;
-//            @Override
-////            public Object executeScript(String string, Object... os) {;
-////                
-////            }
-//
-//            @Override
-//            public Object executeAsyncScript(String string, Object... os) {
-//                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//            }
 
-        String teste = (String) executor.executeScript("document.getElementById(`identificacaoUsuario`).value +=tecladoAlfanumerico.tecla[35].getValor()");
-
-//        document.getElementById("identificacaoUsuario").value +=tecladoAlfanumerico.tecla[35].getValor();
         /* Aqui ja foi aberto o navegador, na URL informada acima
         o metodo findElement vai procurar na pagina atraves do parametro informado
         que no caso é o linkText, e tambem é informado o valor do texto que ele vai procurar
         quando ele achar, a função click sera acionado
          */
-//        driver.findElement(By.linkText("Mais informações")).click();
-//        driver.findElement(By.linkText("Continuar para a página da Web (não recomendado)")).click();
+        driver.findElement(By.linkText("Mais informações")).click();
+        driver.findElement(By.linkText("Continuar para a página da Web (não recomendado)")).click();
         /* Configuração para encotrar o caminho do arquivo em excell */
-//        File file = new File("C:\\Users\\gbjesus1\\Documents\\Selenium\\Massa\\GOVWEB_Inter_Mantem_Usuarios_CXA_1o Dia-7219.xlsx");
-//        FileInputStream fis = new FileInputStream(file);
+        File file = new File("C:\\Users\\gbjesus1\\Documents\\Selenium\\Massa\\GOVWEB_Inter_Mantem_Usuarios_CXA_1o Dia-7219.xlsx");
+        FileInputStream fis = new FileInputStream(file);
 //        /* Configurando a leitura do arquivo Excell */
-//        XSSFWorkbook workbook = new XSSFWorkbook(fis);
-//        XSSFSheet sheet = workbook.getSheetAt(0);
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFSheet sheet = workbook.getSheetAt(0);
 //        Row row = sheet.getRow(32);
 //        Cell cell = row.getCell(4);
         // Variaveis que terão valor com os dados vindo da celula do excell para preenchimento
         //do campo GovConta Caixa
-//        String agencia = sheet.getRow(32).getCell(4).getStringCellValue();
-//        String fixoUm = sheet.getRow(32).getCell(5).getStringCellValue();
-//        String fixoDois = sheet.getRow(32).getCell(6).getStringCellValue();
-//        String Data = sheet.getRow(32).getCell(7).getStringCellValue();
+        String agencia = sheet.getRow(32).getCell(4).getStringCellValue();
+        String fixoUm = sheet.getRow(32).getCell(5).getStringCellValue();
+        String fixoDois = sheet.getRow(32).getCell(6).getStringCellValue();
+        String Data = sheet.getRow(32).getCell(7).getStringCellValue();
 ////        /* -------------------------------------------------------------------- */
-//        WebElement govConta = driver.findElement(By.id("numeroGovConta"));
-//        govConta.sendKeys(agencia + fixoUm + fixoDois + Data);
+        WebElement govConta = driver.findElement(By.id("numeroGovConta"));
+        govConta.sendKeys(agencia + fixoUm + fixoDois + Data);
 //
-//        String senha = sheet.getRow(32).getCell(8).getStringCellValue();
-//        driver.findElement(By.id("senhaUsuario")).click();
+        String senha = sheet.getRow(32).getCell(8).getStringCellValue();
+        driver.findElement(By.id("senhaUsuario")).click();
+        for (int i = 0; i < senha.length(); i++) {
+            String indice = "" + senha.charAt(i);
+            // System.out.println(indice);
+            // util.clickButton(".id", "senhaUsuario");
+            switch (indice) {
+                case "1":
+                    executor.executeScript("document.getElementById('senhaUsuario').value +=tecladoAlfanumerico.tecla[0].getValor()");
+                    break;
+            }
+        }
+        driver.findElement(By.linkText("Cadastro de Usuários")).click();
+
 //        WebElement senhaUserd = driver.findElement(By.tagName("area")).findElement(By.id("rr"));
 //        WebElement senhaUser = driver.findElement(By.id("senhaUsuario"));
 //
